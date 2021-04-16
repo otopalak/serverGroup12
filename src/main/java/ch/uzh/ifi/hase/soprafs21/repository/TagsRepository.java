7549262287a42e87b370c87db6e0b58f4ce95816
dennisshushack <dennis.shushack@uzh.ch>
2021-04-16 14:45:41 +0200

    Update TagsRepository.java

--- src/main/java/ch/uzh/ifi/hase/soprafs21/repository/TagsRepository.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/repository/TagsRepository.java
@@ -2,7 +2,9 @@ package ch.uzh.ifi.hase.soprafs21.repository;
 
 import ch.uzh.ifi.hase.soprafs21.entity.Tags;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository("tagsRepository")
 public interface TagsRepository extends JpaRepository<Tags, Long> {
     Tags getTagsByDescription(String description);
 }
