043d5cc78630200a02898f73927e537048c52a37
dennisshushack <dennis.shushack@uzh.ch>
2021-04-24 16:38:34 +0200

    Picture
    Tryout again

--- src/main/java/ch/uzh/ifi/hase/soprafs21/configuration/AmazonConfig.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/configuration/AmazonConfig.java
@@ -6,6 +6,7 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,10 +14,17 @@ import org.springframework.context.annotation.Configuration;
 public class AmazonConfig {
 
     // Passing our AWS Credentials to access it -> Gives us an instance of the amazon Client
+    @Value("AWS_ACCESS_KEY_ID")
+    private String ID;
+
+    @Value("AWS_SECRET_ACCESS_KEY")
+    private String SECRET;
+
     @Bean
     public AmazonS3 s3() {
-        AWSCredentials awsCredentials = new BasicAWSCredentials(System.getenv("AWS_ACCESS_KEY_ID"),
-                System.getenv("AWS_SECRET_ACCESS_KEY"));
+        AWSCredentials awsCredentials = new BasicAWSCredentials(ID,
+                SECRET);
+
 
         return AmazonS3Client.builder().withRegion("eu-central-1").withCredentials(new AWSStaticCredentialsProvider(awsCredentials)).build();
     }
--- src/test/java/ch/uzh/ifi/hase/soprafs21/service/UserServiceIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs21/service/UserServiceIntegrationTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.web.server.ResponseStatusException;
 
