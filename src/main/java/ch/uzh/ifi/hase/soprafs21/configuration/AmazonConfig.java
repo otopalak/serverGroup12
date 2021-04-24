eec73e54831a260d3596cc466182aebb4e28c17e
dennisshushack <dennis.shushack@uzh.ch>
2021-04-24 15:48:35 +0200

    Update AmazonConfig.java
    Next try

--- src/main/java/ch/uzh/ifi/hase/soprafs21/configuration/AmazonConfig.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/configuration/AmazonConfig.java
@@ -15,8 +15,8 @@ public class AmazonConfig {
     // Passing our AWS Credentials to access it -> Gives us an instance of the amazon Client
     @Bean
     public AmazonS3 s3() {
-        AWSCredentials awsCredentials = new BasicAWSCredentials("${aws.access.key}",
-                "${aws.access.key}");
+        AWSCredentials awsCredentials = new BasicAWSCredentials(System.getenv("AWS_ACCESS_KEY_ID"),
+                System.getenv("AWS_SECRET_ACCESS_KEY"));
 
         return AmazonS3Client.builder().withRegion("eu-central-1").withCredentials(new AWSStaticCredentialsProvider(awsCredentials)).build();
     }
