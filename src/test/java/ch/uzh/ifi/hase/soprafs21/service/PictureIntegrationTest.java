fb0988c3449c4269ba2d6f50ebc57c0fd3a6c53a
dennisshushack <dennis.shushack@uzh.ch>
2021-05-02 14:48:25 +0200

    Small changes
    Bug Fixes and Tests

--- src/test/java/ch/uzh/ifi/hase/soprafs21/service/PictureIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs21/service/PictureIntegrationTest.java
@@ -1,16 +1,23 @@
 package ch.uzh.ifi.hase.soprafs21.service;
 
 
+import ch.uzh.ifi.hase.soprafs21.entity.Item;
+import ch.uzh.ifi.hase.soprafs21.repository.ItemRepository;
 import ch.uzh.ifi.hase.soprafs21.repository.PictureDBRepository;
+import com.amazonaws.auth.AnonymousAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import org.apache.http.entity.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mapstruct.Qualifier;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
@@ -20,11 +27,9 @@ import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
+
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.*;
@@ -37,11 +42,26 @@ public class PictureIntegrationTest {
     private PictureDBRepository pictureDBRepository;
 
     @Autowired
+    private ItemRepository itemRepository;
+
+    @Autowired
+    private ItemService itemService;
+
+
+    @Autowired
     private PictureStorageService pictureStorageService;
 
     @Autowired
     private WebApplicationContext wac;
     private MockMvc mockMvc;
+
+
+    @BeforeEach
+    public void setup(){
+        itemRepository.deleteAll();
+        pictureDBRepository.deleteAll();
+    }
+
 
     /*
      * THis test checks the upload of a picture -> Here we do not have an item, that goes with it, that is why
@@ -56,6 +76,19 @@ public class PictureIntegrationTest {
         Exception exception = assertThrows(ResponseStatusException.class, () -> pictureStorageService.uploadItemImage(1L, multiPFImage));
         assertEquals(exception.getMessage(),HttpStatus.CONFLICT+ " \"The item does not exist, thus you cannot add pictues!\"");
 
+     }
+
+     // Here we check, that only a image type is allowed and not a Filetype different to JPEG,PNG and GIF
+    @Test
+    public void upload_picture_invalidInput_wrongInputType(){
+        // Creates a Mock Multipart File
+        MockMultipartFile multiPFImage = new MockMultipartFile("abcpic.txt", "abcpic.txt",
+                "text/plain", "Generate bytes to simulate a picture".getBytes());
+
+        Exception exception = assertThrows(ResponseStatusException.class, () -> pictureStorageService.uploadItemImage(1L, multiPFImage));
+        assertEquals(exception.getMessage(),HttpStatus.CONFLICT+ " \"Your filetype is not a JPEG,PNG or a GIF\"");
+
     }
+    
     }
 
